5128fcf2e25eff7d6999438e41b677d1c46df916
Okoli Jahbuchim <jahbuchimokoli3@gmail.com>
2022-10-07 22:06:37 +0100

    Transitons added to sidebar

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -9,28 +9,30 @@ const minorState = minorStateData();
 </script>
 
 <template>
-  <div
-    v-if="minorState.show_side_bar"
-    class="bg-black w-full h-screen z-40 bg-opacity-30 absolute top-0"
-  >
+  <TransitionGroup name="list">
     <div
-      class="bg-white h-screen absolute z-50 top-0 left-0 border-r border-gray-100 w-full max-w-[240px] shadow"
+      v-if="minorState.show_side_bar"
+      class="bg-black w-full h-screen z-40 bg-opacity-30 absolute top-0"
     >
-      <div class="h-14">
-        <LogoVue class="transform -translate-y-2"/>
-      </div>
-      <div class="my-4">
-        <div
-          v-for="(icons, index) in sideBarLinks"
-          :key="index"
-          class="flex items-center space-x-6 px-5 hover:bg-gray-100 py-2 cursor-pointer"
-        >
-          <span class="material-symbols-outlined text-md font-light">
-            {{ icons.icon }}
-          </span>
-          <span class="font-light text-sm">{{ icons.text }}</span>
+      <div
+        class="bg-white h-screen absolute z-50 top-0 left-0 border-r border-gray-100 w-full max-w-[240px] shadow"
+      >
+        <div class="h-14">
+          <LogoVue class="transform -translate-y-2" />
+        </div>
+        <div class="my-4">
+          <div
+            v-for="({ icon, text }, index) in sideBarLinks"
+            :key="index"
+            class="flex items-center space-x-6 px-5 hover:bg-gray-100 py-2 cursor-pointer"
+          >
+            <span class="material-symbols-outlined text-md font-light">
+              {{ icon }}
+            </span>
+            <span class="font-light text-sm">{{ text }}</span>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </TransitionGroup>
 </template>
